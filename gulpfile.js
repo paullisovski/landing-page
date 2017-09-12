fa0c0a1bf38c8abe6f259afaa9cb89eb578b0062
Pavel Lisovsky <paullisovski@gmail.com>
2017-09-12 14:52:51 +0300

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp         = require('gulp'),
+    sass         = require('gulp-sass'),
+    browserSync  = require('browser-sync'),
+    cssmin       = require('gulp-cssmin'),
+    rename       = require('gulp-rename'),
+    autoprefixer = require('gulp-autoprefixer');
+
+
+gulp.task('sass', function(){
+    return gulp.src('src/styles/sass/**/*.sass')
+        .pipe(sass())
+        .pipe(gulp.dest('src/styles/css'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: './'
+        },
+        notify: false
+    });
+});
+
+gulp.task('css-min', ['sass'], function() {
+    return gulp.src('src/styles/css/main.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('src/styles/css'));
+});
+
+gulp.task('watch', ['browser-sync', 'sass'], function() {
+    gulp.watch('src/styles/sass/**/*.sass', ['sass']);
+    gulp.watch('index.html', browserSync.reload);
+});
